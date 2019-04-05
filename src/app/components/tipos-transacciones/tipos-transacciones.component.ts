347c35f734bacf1d8de29222bcf0aedcd45ac74a
CarlosLara <carlosdaniellarsol@gmail.com>
2019-04-04 22:21:24 -0600

    Componentes actualizados faltan 30,33,35 y 39 en adelante

---
+++ src/app/components/tipos-transacciones/tipos-transacciones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tipos-transacciones',
+  templateUrl: './tipos-transacciones.component.html',
+  styleUrls: ['./tipos-transacciones.component.css']
+})
+export class TiposTransaccionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
